843d95f31f2d91ae57115c8d62cb40af963a30a0
tya6i <developertyagi1203@gmail.com>
2020-06-30 16:31:38 +0530

    edit

--- src/components/Alertreact.js
+++ src/components/Alertreact.js
@@ -6,6 +6,9 @@ function Alertreact() {
             <h1>Add some another file</h1>
             <h2>How it is possible ?</h2>
             <h3>let's add some another</h3>
+            <form>
+               Enter yoyr name: <input type="text" />
+                </form>
             </div>
     );
 }
