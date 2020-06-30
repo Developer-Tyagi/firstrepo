7680d89787e2e427ee43f7d575d4f27ce0d2d091
tya6i <developertyagi1203@gmail.com>
2020-06-30 16:14:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Alertreact from './components/Alertreact'
+import './App.css';
+
+function App() {
+      
+  return (
+    <div className="App">
+      <h1>Tyagi</h1>
+      <Alertreact />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Alertreact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Alertreact() {
+    return(
+        <div>
+            <h1>Add some another file</h1>
+            <h2>How it is possible ?</h2>
+            <h3>let's add some another</h3>
+            </div>
+    );
+}
+
+export default Alertreact
